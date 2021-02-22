db0c5aaa83e74ccd0e2808bc817c917286a66306
longtao <eustancewu@gmail.com>
2021-02-22 00:12:10 -0800

    start ignited

--- src/ztest/host/main.py
+++ src/ztest/host/main.py
@@ -5,6 +5,9 @@ import init_cmd
 import build_image_cmd
 import update_image_cmd
 
+from utils import bash, misc
+import psutil
+
 commands = [
     init_cmd.InitCmd(),
     run_test_cmd.RunTest(),
@@ -14,6 +17,26 @@ commands = [
 ]
 
 
+def start_ignited_if_not():
+    is_running = False
+
+    for p in psutil.process_iter():
+        cmdline = p.cmdline()
+        if 'ignited' in cmdline:
+            is_running = True
+            break
+
+    if not is_running:
+        bash.call_with_screen_output('nohup ignited daemon --log-level debug &')
+
+        @misc.retry(5, 0.5)
+        def wait_for_start():
+            bash.call('ignite ps')
+
+        wait_for_start()
+
+
 def main():
+    start_ignited_if_not()
     cli.run_command(commands)
 
