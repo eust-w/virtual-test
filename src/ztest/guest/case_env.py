a69c93d7ba3e4f5201ac58c2bbf8a4b0dabc0286
longtao <eustancewu@gmail.com>
2021-02-22 22:21:24 -0800

    add some case environment variables

--- src/ztest/guest/case_env.py
+++ src/ztest/guest/case_env.py
@@ -4,6 +4,7 @@ import os
 TEST_ROOT = env.env_var('ztest.case.testRoot', str, '/root/test_root')
 IMAGE_PATH = env.env_var('ztest.case.imagePath', str, os.path.join(TEST_ROOT.value(), 'images/zstack-image-1.4.qcow2'))
 VOLUME_DIR = env.env_var('ztest.case.volumeDir', str, os.path.join(TEST_ROOT.value(), 'volumes'))
+SNAPSHOT_DIR = env.env_var('ztest.case.snapshotDir', str, os.path.join(TEST_ROOT.value(), 'snapshots'))
 DEFAULT_ETH_INTERFACE_NAME = env.env_var('ztest.case.defaultEthInterfaceName', str, 'eth0')
 
 
@@ -11,7 +12,9 @@ env_variables_for_test_case = [
     TEST_ROOT,
     IMAGE_PATH,
     VOLUME_DIR,
+    SNAPSHOT_DIR,
     DEFAULT_ETH_INTERFACE_NAME,
+    env.ZSTACK_UTILITY_SRC_IN_VM
 ]
 
 
